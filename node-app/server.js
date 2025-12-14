66c4a43a7a038ad49c54441324452e1e145fe68d
Brent Menos <2241577@slu.edu.ph>
2025-12-14 19:47:37 +0800

    removed redundunt variable

--- node-app/server.js
+++ node-app/server.js
@@ -5,7 +5,6 @@ const session = require('express-session');
 const multer = require('multer');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
